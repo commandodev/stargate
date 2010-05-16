77552da66433723dcfa735edb3a4c38ca9b18997
Ben Ford <ben.fordnz@gmail.com>
2010-05-16 07:45:05 +0100

    Changed tests to only set the server up once (a module fuxture) and tried fr a better buildout to get coverage tests on hudson

--- tests/test_websocket.py
+++ tests/test_websocket.py
@@ -65,21 +65,9 @@ def not_found(context, request):
 
 ##### Borrowed from the eventlet tests package
 
-class TestIsTakingTooLong(Exception):
-    """ Custom exception class to be raised when a test's runtime exceeds a limit. """
-    pass
+class Fixture(object):
 
-class LimitedTestCase(TestCase):
-    """ Unittest subclass that adds a timeout to all tests.  Subclasses must
-    be sure to call the LimitedTestCase setUp and tearDown methods.  The default
-    timeout is 1 second, change it by setting self.TEST_TIMEOUT to the desired
-    quantity."""
-
-    TEST_TIMEOUT = 2
-    SERVE = server_factory({}, 'localhost', 6544)
-
-    def setUp(self):
-        self.timer = None
+    def setUp(self, module):
         config = testing.setUp()
         config._set_root_factory(get_root)
         config_logger = getLogger("config")
@@ -95,8 +83,8 @@ class LimitedTestCase(TestCase):
         self.logfile = StringIO()
         self.killer = None
         self.spawn_server()
-        eventlet.sleep(0.5)
-        self.set_timeout(self.TEST_TIMEOUT)
+        eventlet.sleep(0.3)
+
 
     def set_timeout(self, new_timeout):
         """Changes the timeout duration; only has effect during one test case"""
@@ -124,7 +112,7 @@ class LimitedTestCase(TestCase):
         self.port = sock.getsockname()[1]
         self.killer = eventlet.spawn_n(wsgi.server, sock, app, **new_kwargs)
 
-    def tearDown(self):
+    def tearDown(self, module):
 #        self.timer.cancel()
         greenthread.kill(self.killer)
         eventlet.sleep(0)
@@ -144,6 +132,97 @@ class LimitedTestCase(TestCase):
 
         eventlet.sleep(0)
 
+class TestIsTakingTooLong(Exception):
+    """ Custom exception class to be raised when a test's runtime exceeds a limit. """
+    pass
+
+class LimitedTestCase(TestCase):
+    """ Unittest subclass that adds a timeout to all tests.  Subclasses must
+    be sure to call the LimitedTestCase setUp and tearDown methods.  The default
+    timeout is 1 second, change it by setting self.TEST_TIMEOUT to the desired
+    quantity."""
+
+    TEST_TIMEOUT = 2
+    SERVE = server_factory({}, 'localhost', 6544)
+
+    def setUp(self):
+        self.timer = None
+        self.port = fixture.port
+        self.set_timeout(self.TEST_TIMEOUT)
+
+    def tearDown(self):
+#        self.timer.cancel()
+        eventlet.sleep(0)
+        if self.timer:
+            self.timer.cancel()
+            #greenthread.kill(self.timer)
+
+#    def setUp(self):
+#        self.timer = None
+#        config = testing.setUp()
+#        config._set_root_factory(get_root)
+#        config_logger = getLogger("config")
+#        config_logger.setLevel(logging.INFO)
+#        config.add_route('echo', '/echo', EchoWebsocket)
+#        config.add_route('range', '/range', RangeWebsocket)
+#        config.add_view(name='traversal_echo', context=Root,
+#                        view=EchoWebsocket, custom_predicates=[is_websocket])
+#        # Add a not found view as setup_registry won't have been called
+#        config.add_view(view=not_found, context=NotFound)
+#        config.end()
+#        self.config = config
+#        self.logfile = StringIO()
+#        self.killer = None
+#        self.spawn_server()
+#        eventlet.sleep(0.5)
+#        self.set_timeout(self.TEST_TIMEOUT)
+#
+    def set_timeout(self, new_timeout):
+        """Changes the timeout duration; only has effect during one test case"""
+        if self.timer:
+            self.timer.cancel()
+        self.timer = eventlet.Timeout(new_timeout,
+                                      TestIsTakingTooLong(new_timeout))
+#
+#    def spawn_server(self, **kwargs):
+#        """Spawns a new wsgi server with the given arguments.
+#        Sets self.port to the port of the server, and self.killer is the greenlet
+#        running it.
+#
+#        Kills any previously-running server."""
+#        if self.killer:
+#            greenthread.kill(self.killer)
+#            eventlet.sleep(0)
+#        app = self.config.make_wsgi_app()
+#        new_kwargs = dict(max_size=128,
+#                          log=self.logfile)
+#        new_kwargs.update(kwargs)
+#
+#        sock = eventlet.listen(('localhost', 0))
+#
+#        self.port = sock.getsockname()[1]
+#        self.killer = eventlet.spawn_n(wsgi.server, sock, app, **new_kwargs)
+#
+#    def tearDown(self):
+##        self.timer.cancel()
+#        greenthread.kill(self.killer)
+#        eventlet.sleep(0)
+#        if self.timer:
+#            self.timer.cancel()
+#            #greenthread.kill(self.timer)
+#        try:
+#            hub = hubs.get_hub()
+#            num_readers = len(hub.get_readers())
+#            num_writers = len(hub.get_writers())
+#            assert num_readers == num_writers == 0
+#        except AssertionError, e:
+##            set_trace()
+#            print "ERROR: Hub not empty"
+#            print debug.format_hub_timers()
+#            print debug.format_hub_listeners()
+#
+#        eventlet.sleep(0)
+
     @raises(urllib2.HTTPError)
     def test_incorrect_headers(self):
         try:
@@ -154,8 +233,6 @@ class LimitedTestCase(TestCase):
 
     @raises(urllib2.HTTPError)
     def test_traversal_view_lookup(self):
-        print '\n', self.port, '\n'
-        #eventlet.sleep(60)
         try:
             urllib2.urlopen("http://localhost:%s/traversal_echo" % self.port)
         except urllib2.HTTPError, e:
@@ -299,5 +376,7 @@ class LimitedTestCase(TestCase):
         fd.flush()
         sock.close()
 
-
+fixture = Fixture()
+setup_module = fixture.setUp
+teardown_modulw = fixture.tearDown
 
