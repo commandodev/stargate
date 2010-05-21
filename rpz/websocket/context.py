7c379336c0527e0ea4c1aaa16dd6cbcd996cbee4
Ben Ford <ben.fordnz@gmail.com>
2010-05-21 07:01:44 +0100

    Added a basic context object that can keep track of websocket connections

---
+++ rpz/websocket/context.py
@@ -0,0 +1,54 @@
+"""This module supplies a class which can be subclassed or mixed in and used in
+conjunction with :class:`~rpz.websocket.WebSocketView`. It provides
+functionality for adding and removing views as listeners for events
+"""
+
+from eventlet.green import socket
+from eventlet.support import get_errno
+from repoze.bfg.traversal import model_path
+import errno
+
+class ListenersDescriptor(object):
+
+    def __init__(self):
+        self._registered = set()
+
+    def __get__(self, obj, klass=None):
+        if obj:
+            return self._registered
+
+
+class WebSocketAwareContext(object):
+
+    listeners = ListenersDescriptor()
+
+    __name__ = ''
+    __parent__ = None
+
+    def __str__(self):
+        return u"Node: %s (%s)" % (self.__name__ if self.__name__ else 'ROOT',
+                                 self.path)
+
+    @property
+    def path(self):
+        return model_path(self)
+
+    def add_listener(self, ws):
+        """Adds a :class:`mulitvisor.server.websocket.Websocket` the the set of listeners"""
+        self.listeners.add(ws)
+
+    def remove_listener(self, ws):
+        """Removes ws from the set of listeners"""
+        self.listeners.discard(ws)
+
+    def send(self, message):
+        remove = []
+        for ws in self.listeners:
+            try:
+                ws.send(message)
+            except socket.error, e: #pragma NO COVER
+                if get_errno(e) != errno.EPIPE:
+                    raise
+                remove.append(ws)
+        for ws in remove:
+            self.remove_listener(ws)
---
+++ tests/test_context.py
@@ -0,0 +1,75 @@
+from eventlet.green import socket
+from eventlet.websocket import WebSocket
+from nose.tools import *
+from rpz.websocket.context import WebSocketAwareContext
+from unittest import TestCase
+import mock
+
+class Traverser(WebSocketAwareContext):
+
+    def __init__(self):
+        self._router = dict()
+
+    def __getitem__(self, key):
+        return self._router[key]
+
+class BrokenWebSocket(WebSocket):
+
+    def send(self, msg):
+        exc = socket.error()
+        exc.errno = 32 #EPIPE
+        raise exc
+
+class TestWebSocketAwareContext(TestCase):
+
+    def setUp(self):
+        self.ctx = WebSocketAwareContext()
+        self.mock_socket = s = mock.Mock()
+        self.environ = env = dict(HTTP_ORIGIN='http://localhost', HTTP_WEBSOCKET_PROTOCOL='ws',
+                                  PATH_INFO='test')
+
+        self.test_ws = WebSocket(s, env)
+
+    def test_context_has_listeners(self):
+        eq_(self.ctx.listeners, set())
+
+    def test_context_add_listeners(self):
+        ws = self.test_ws
+        OTHER_WS = WebSocket(self.mock_socket, self.environ)
+        ctx = self.ctx
+        eq_(ctx.listeners, set())
+        ctx.add_listener(ws)
+        eq_(ctx.listeners, set([ws]))
+        ctx.add_listener(ws)
+        eq_(ctx.listeners, set([ws]))
+        ctx.remove_listener(OTHER_WS)
+        eq_(ctx.listeners, set([ws]))
+        ctx.remove_listener(ws)
+        eq_(ctx.listeners, set())
+
+    def test_context_send_to_ws(self):
+        ctx = self.ctx
+        ws = self.test_ws
+        ctx.add_listener(ws)
+        ctx.send('hello')
+        ok_(ws.socket.sendall.called_with("\x00hello\xFF"))
+
+    def test_model_path(self):
+        root = Traverser()
+        child = Traverser()
+        child.__parent__ = root
+        child.__name__ = 'child'
+        eq_(child.path, '/child')
+        eq_(str(child), 'Node: child (/child)')
+
+    def test_context_remove_broken_pipe(self):
+        ws = self.test_ws
+        OTHER_WS = BrokenWebSocket(self.mock_socket, self.environ)
+        ctx = self.ctx
+        eq_(ctx.listeners, set())
+        ctx.add_listener(ws)
+        ctx.add_listener(OTHER_WS)
+        ctx.send('hello')
+        ok_(ws.socket.sendall.called_with("\x00hello\xFF"))
+        # The broken socket should have been removed
+        eq_(ctx.listeners, set([ws]))
