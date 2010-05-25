209d4b320982efef48372f9c1f03e46f4c8ed974
Ben Ford <ben.fordnz@gmail.com>
2010-05-25 06:38:51 +0100

    Remove scaffold test

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -4,12 +4,6 @@ import mock
 
 INI_FILE = os.path.join(os.path.dirname(__file__), 'test.ini')
 
-def test_ini_file():
-    open(INI_FILE, 'r').read() == """[server:main]
-use = egg:rpz.websocket#eventlet_server
-host = 0.0.0.0
-port = 6544"""
-
 @mock.patch('eventlet.listen')
 @mock.patch('eventlet.wsgi.server')
 def test_loadserver(server_patch, listen_patch):
