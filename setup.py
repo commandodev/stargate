5f7a9888767005ec6892896c02b23cce69aa1291
Ben Ford <ben@boothead.co.uk>
2011-02-11 09:39:31 +0000

    Give history an 'rst' extension so it can me used in sphinx

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='stargate',
       version=version,
       description="Real time communication for pyramid using WebSockets",
       long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
